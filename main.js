79a30f5dafb46aa7d8a7b81a619a156a2afeb529
djogeology <bzwajih@gmail.com>
2024-04-15 14:19:46 +0100

    films images and trailers dowon

---
+++ main.js
@@ -0,0 +1,18 @@
+const ActionFilm=[{title:"Die Hard 1988"}, 
+{title:"Mad Max Fury Road 2015"},
+{title:"The Dark Knight 2008"},
+{title:"The Raid 2011"},
+{title:"John Wick 2014"}
+]
+const RomanceFilm=[{title:"Casablanca 1942"}, 
+{title:"The Notebook 2004"},
+{title:"Pride and Prejudice 2005"},
+{title:"Before Sunrise 1995"},
+{title:"Eternal Sunshine of the Spotless Mind 2004 "}
+]
+const ComedyFilm=[{title:"The Hangover 2009"}, 
+{title:"Superbad 2007"},
+{title:"Bridesmaids 2011"},
+{title:"Knives Out 2019"},
+{title:"The Grand Budapest Hotel 2014"}
+]
